2925fc79a88be87470a3f626715182156a50e5c0
Brandt Kruger <brandt.kruger087@gmial.com>
2020-01-12 20:54:46 +0200

    Add validation for controller test.

--- productsservice/src/test/java/com/kruger/productsservice/controller/ProductControllerTest.java
+++ productsservice/src/test/java/com/kruger/productsservice/controller/ProductControllerTest.java
@@ -1,18 +1,22 @@
 package com.kruger.productsservice.controller;
 
+import com.kruger.productsservice.model.Product;
 import com.kruger.productsservice.services.ProductService;
 import com.kruger.productsservice.services.ProductServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class ProductControllerTest {
 
@@ -39,9 +43,31 @@ class ProductControllerTest {
 
     @Test
     void getAllProducts() {
+
+        Set<Product> productSet = new HashSet<>();
+
+        Product product = new Product();
+        product.setId(1L);
+        product.setManufacturer("A");
+        product.setInStock(10);
+        product.setDescription("1");
+        productSet.add(product);
+
+        Product product1 = new Product();
+        product1.setId(2L);
+        product1.setDescription("2");
+        product1.setInStock(3);
+        product1.setManufacturer("B");
+        productSet.add(product1);
+
+        when(productService.getAllProducts()).thenReturn(productSet);
+        ArgumentCaptor<Set<Product>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         final String viewAllProducts = productController.getAllProducts(model);
         assertEquals("allproducts", viewAllProducts);
         verify(productService, times(1)).getAllProducts();
-        verify(model, times(1)).addAttribute(eq("products"), anySet());
+        verify(model, times(1)).addAttribute(eq("products"), argumentCaptor.capture());
+        Set<Product> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
