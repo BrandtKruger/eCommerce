146816603019b016816550e61e67cfbdff8ad7c9
Brandt Kruger <brandt.kruger087@gmial.com>
2020-02-09 20:04:54 +0200

    Remove File.

---
+++ productsservice/src/test/java/com/kruger/productsservice/services/ProductServiceTest.java
@@ -0,0 +1,28 @@
+package com.kruger.productsservice.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getProductsByCategory() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+}
